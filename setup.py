4d5870acd264545d7626ddb2a7874704ffbb1a36
Frédéric van der Essen <fvdessen@gmail.com>
2015-06-15 23:24:05 +0200

    bumping version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
         name             = "odoo-ook",
-        version          = "2.0.0",
+        version          = "3.0.0",
         author           = "Frederic van der Essen",
         author_email     = "fvdessen+x@gmail.com",
         packages         = ["ook"],
